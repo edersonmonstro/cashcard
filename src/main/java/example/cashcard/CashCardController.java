29a8234a6a91c43fde255df9c8ea72c2c8ed6c75
Edersonmonstro <edersonmedeiros@gmail.com>
2023-10-04 08:53:34 -0400

    end of chapter lab: returning a list with GET

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -1,33 +1,27 @@
 package example.cashcard;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Optional;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.*;
 
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
-
     private CashCardRepository cashCardRepository;
 
-    public CashCardController(CashCardRepository cashCardRepository){
+    public CashCardController(CashCardRepository cashCardRepository) {
         this.cashCardRepository = cashCardRepository;
     }
 
     @GetMapping("/{requestedId}")
     public ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        //return ResponseEntity.ok("{}");
-       
-        // if(requestedId.equals(99L)){
-        //     CashCard cashCard = new CashCard(99L,123.45);
-        //     return ResponseEntity.ok(cashCard);
-        // } else {
-        //     return ResponseEntity.notFound().build();
-        // }
-       
         Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
         if (cashCardOptional.isPresent()) {
             return ResponseEntity.ok(cashCardOptional.get());
@@ -35,4 +29,38 @@ public class CashCardController {
             return ResponseEntity.notFound().build();
         }
     }
- }
+
+    @PostMapping
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
+        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+        URI locationOfNewCashCard = ucb
+                .path("cashcards/{id}")
+                .buildAndExpand(savedCashCard.id())
+                .toUri();
+        return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+    // @GetMapping()
+    // public ResponseEntity<Iterable<CashCard>> findAll() {
+    //     return ResponseEntity.ok(cashCardRepository.findAll());
+    // }
+
+    /**
+     * 
+     * The default values of page is 0 and size is 20 provided by spring if not specified.
+     * Default behavier for sort is already coded with getSortOr -> amount asc
+     * @param pageable
+     * @return
+     * 
+     */
+    @GetMapping
+    public ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
+        Page<CashCard> page = cashCardRepository.findAll(
+                PageRequest.of(
+                        pageable.getPageNumber(),
+                        pageable.getPageSize(),
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))
+        ));
+        return ResponseEntity.ok(page.getContent());
+    }
+}
